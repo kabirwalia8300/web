5c7aca706e8485c995f4e811a539a9c14dbf4e97
kabirwalia8300 <kabirwalia55@gmail.com>
2019-08-17 12:12:08 -0500

    Updates

---
+++ precache-manifest.a1fedc4bc804f75fdd923d9c01177c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5de4b85724b299b95f5d828e370cf647",
+    "url": "/web/index.html"
+  },
+  {
+    "revision": "1eb003bfdc970b10a47f",
+    "url": "/web/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2db24df62f4e141391a6",
+    "url": "/web/static/js/2.aba38f98.chunk.js"
+  },
+  {
+    "revision": "1eb003bfdc970b10a47f",
+    "url": "/web/static/js/main.e0820e50.chunk.js"
+  },
+  {
+    "revision": "e0b9f366892bc5228713",
+    "url": "/web/static/js/runtime~main.efcc67c2.js"
+  }
+]);
